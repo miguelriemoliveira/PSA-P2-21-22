e7b44ad026698dd8b7eb59916f6b7ebf96f3b2d0
Miguel Oliveira <m.riem.oliveira@gmail.com>
2022-05-30 10:55:05 +0100

    Camera image wider fov.

--- psa_robot_description/scripts/driver.py
+++ psa_robot_description/scripts/driver.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 
 # rospy for the subscriber
+from copy import deepcopy
+
 import rospy
 
 # OpenCV2 for saving an image
@@ -24,20 +26,51 @@ def imageCallback(msg):
 
     try:
         # Convert your ROS Image message to OpenCV2
-        cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+        image_rgb = bridge.imgmsg_to_cv2(msg, "bgr8")
     except:
         print('Could not convert image')
         return
 
+    # Preprocessing
+    image_gui = deepcopy(image_rgb)
+    image_gray = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2GRAY)  # convert to grayscale
+    _, image_thresh = cv2.threshold(image_gray, 127, 255, cv2.THRESH_BINARY)  # thresholding
+
     # Save your OpenCV2 image as a jpeg
-    cv2.imshow('front_camera', cv2_img)
-    cv2.waitKey(20)
 
     # How to process the image to define the best angle and speed?
+    reference_y = 190
+    height, width = image_thresh.shape
+    middle_x = int(width / 2)
+
+    # search from middle to right
+    right_xs = []
+    for x in range(middle_x, width):
+        if image_thresh[reference_y, x] == 255:
+            right_xs.append(x)
+
+    # search from middle to left
+    left_xs = []
+    for x in range(middle_x-1, -1, -1):
+        if image_thresh[reference_y, x] == 255:
+            left_xs.append(x)
+
+    # Drawing
+    for x in right_xs:
+        cv2.line(image_gui, (x, reference_y), (x, reference_y), (0, 0, 255), 4)
+
+    for x in left_xs:
+        cv2.line(image_gui, (x, reference_y), (x, reference_y), (255, 0, 0), 4)
 
     # make a driving decision
     angle = 0
-    speed = 0.1
+    speed = 0
+
+    # cv2.imshow('image_rgb', image_rgb)
+    # cv2.imshow('image_gray', image_gray)
+    # cv2.imshow('image_thresh', image_thresh)
+    cv2.imshow('image_gui', image_gui)
+    cv2.waitKey(20)
 
     # build a twist msg to publish
     twist = Twist()
