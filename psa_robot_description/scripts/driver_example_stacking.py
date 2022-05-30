2125aebab06c59d8a81d9305c5fcf5fb5840a921
Daniel Coelho <danielsilveiracoelho@ua.pt>
2022-05-30 10:53:39 +0100

    #3; add vertical stacking function

---
+++ psa_robot_description/scripts/driver_example_stacking.py
@@ -0,0 +1,70 @@
+#! /usr/bin/python3
+
+# rospy for the subscriber
+import rospy
+
+# OpenCV2 for saving an image
+import cv2
+from geometry_msgs.msg import Twist
+
+# ROS Image message
+from sensor_msgs.msg import Image
+
+# ROS Image message -> OpenCV2 image converter
+from cv_bridge import CvBridge, CvBridgeError
+
+from psa_robot_description.src.vertical_stacking import verticalStacking
+
+
+# Global variables
+# Instantiate CvBridge
+bridge = CvBridge()
+publisher = None
+
+
+def imageCallback(msg):
+    print("Received an image!")
+
+    try:
+        # Convert your ROS Image message to OpenCV2
+        cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+    except:
+        print('Could not convert image')
+        return
+
+    # Save your OpenCV2 image as a jpeg
+    cv2.imshow('front_camera', cv2_img)
+    cv2.waitKey(20)
+
+    # How to process the image to define the best angle and speed?
+    stacked = verticalStacking(image = cv2_img, y_limits = [250,280])
+    
+    
+
+    # make a driving decision
+    angle = 0
+    speed = 0.1
+
+    # # build a twist msg to publish
+    # twist = Twist()
+    # twist.linear.x = speed
+    # twist.angular.z = angle
+    # global publisher
+    # publisher.publish(twist)
+
+
+def main():
+    rospy.init_node('driver')
+    # Define your image topic
+    image_topic = "/front_camera/rgb/image_raw"
+    # Set up your subscriber and define its callback
+    rospy.Subscriber(image_topic, Image, imageCallback)
+    global publisher
+    publisher = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+
+    # Spin until ctrl + c
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
